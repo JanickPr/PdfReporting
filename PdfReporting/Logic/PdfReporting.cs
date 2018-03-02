769802ad503afc91f97fdeb02f6758c74fad0cf2
janick preun <janick-preun@t-online.de>
2018-03-02 13:09:09 +0100

    elementheightcalculation added, exception for panels added,

--- PdfReporting/Logic/PdfReporting.cs
+++ PdfReporting/Logic/PdfReporting.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Media;
 
@@ -14,8 +15,9 @@ namespace PdfReporting.Logic
     {
         #region Fields
 
-        private static double pageHight = 1123.2;
-        private static double pageWidth = 796.8;
+        private static double standartPageHeight = 1123.2;
+        private static double standartPageWidth = 796.8;
+        private static int length;
 
         #endregion
 
@@ -50,20 +52,122 @@ namespace PdfReporting.Logic
         /// <summary>
         /// Goes through the reports visualtree and divides the report into several pages.
         /// </summary>
-        /// <param name="pdfReport"></param>
+        /// <param name="frameworkElement"></param>
         /// <returns></returns>
-        private static List<FixedPage> DivideReportIntoPages(PdfReport pdfReport)
+        private static List<FixedPage> DivideReportIntoPages(FrameworkElement frameworkElement)
         {
-            var pagesList = new List<FixedPage>();
-            double actualPageHeight = 0;
-            int maxPageCount =  Convert.ToInt32(Math.Ceiling(pdfReport.ActualHeight / pageHight));
-
-            for(int pageCounter = 0; pageCounter < maxPageCount)
+            if (frameworkElement == null)
             {
-
+                throw new ArgumentNullException(nameof(frameworkElement));
             }
 
+            FrameworkElement pageContent;
+            FrameworkElement currentFrameworkElement = frameworkElement;
+
+            double cumulatedHeight = 0;
+
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(currentFrameworkElement); i++)
+            {
+                double elementHeightContentIncluded = GetFrameworkElementHeightIncludingContent(currentFrameworkElement);
+
+                if(cumulatedHeight + elementHeightContentIncluded < standartPageHeight)
+                {
+                    //The full Element is added to pageContent at the correct position
+                }
+
+                if(IsSameOrSubclass(typeof(Panel), currentFrameworkElement.GetType()) && 
+                    PanelHasVerticalOrientedItems((Panel)currentFrameworkElement))
+                {
+                    //We must iterate through all items because the Panle can be distributet over several pages.
+                }
+            }
+
+            var pagesList = new List<FixedPage>();
+
+           
+
             return pagesList;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="panel"></param>
+        /// <returns></returns>
+        private static bool PanelHasVerticalOrientedItems(Panel panel)
+        {
+            if (panel.GetType() == typeof(Grid))
+            {
+                Grid grid = (Grid)panel;
+                return grid.RowDefinitions.Count > 0;
+            }
+
+            return panel.LogicalOrientationPublic == Orientation.Vertical;
+
+            //Here we must check for other paneltypes e.g. Canvas or WrapPanel
+        }
+
+        /// <summary>
+        /// Returns the height that is used by the given frameworkelement with the content included.
+        /// </summary>
+        /// <param name="frameworkElement"></param>
+        /// <returns></returns>
+        private static double GetFrameworkElementHeightIncludingContent(FrameworkElement frameworkElement)
+        {
+            double height = GetFrameworkElementHeightExcludingContent(frameworkElement);
+
+            height += frameworkElement.ActualHeight;
+
+            return height;
+        }
+
+        /// <summary>
+        /// Returns the height that is used by the given frameworkelement without the content.
+        /// </summary>
+        /// <param name="frameworkElement"></param>
+        /// <returns></returns>
+        private static double GetFrameworkElementHeightExcludingContent(FrameworkElement frameworkElement)
+        {
+            double height = 0;
+
+            height += GetHeightFromSpacingproperty(frameworkElement.Margin);
+
+            if (frameworkElement.GetType().GetProperty("Padding") != null)
+            {
+                var padding = frameworkElement.GetType().GetProperty("Padding").GetValue(frameworkElement);
+                height += GetHeightFromSpacingproperty((Thickness)padding);
+            }
+
+            if (frameworkElement.GetType().GetProperty("BorderThickness") != null)
+            {
+                var borderThickness = frameworkElement.GetType().GetProperty("BorderThickness").GetValue(frameworkElement);
+                height += GetHeightFromSpacingproperty((Thickness)borderThickness);
+            }
+
+            return height;
+        }
+
+        /// <summary>
+        /// returns the vertical space that is used for spacing such as margin or padding.
+        /// </summary>
+        /// <param name="spacing"></param>
+        /// <returns></returns>
+        private static double GetHeightFromSpacingproperty(Thickness spacing)
+        {
+            return spacing.Top + spacing.Bottom;
+        }
+
+        /// <summary>
+        /// Returns true if the given descendant type derives from the given base type
+        /// or if the decendant type is equal to the base type.
+        /// </summary>
+        /// <param name="Base"></param>
+        /// <param name="Descendant"></param>
+        /// <returns></returns>
+        private static bool IsSameOrSubclass(Type Base, Type Descendant)
+        {
+            return Descendant.IsSubclassOf(Base)
+                   || Descendant == Base;
         }
 
         #endregion
